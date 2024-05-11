ba2d5ca1793b5a4d6fe1022473a8bfed3e9abab7
orhgool <orhgool@gmail.com>
2024-05-10 21:22:22 -0600

    10-05-2024 2122hrs ALLOWED_HOSTS

--- alfa/deployment.py
+++ alfa/deployment.py
@@ -3,7 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], 'alfadte.azurewebsites.net']
+#ALLOWED_HOSTS = ['alfadte.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
@@ -39,23 +40,9 @@ DATABASES = {
 		#'USER': parameters['user'],
 		#'PASSWORD': parameters['password'],
 		'NAME': os.environ.get('DB_NAME', 'postgres'),
-        'USER': os.environ.get('DB_USER', 'marl0nH'), # jqiajkthdz
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Ammh0909$'), # YGOR24H577XH22G2$
+        'USER': os.environ.get('DB_USER', 'marl0nH'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'Ammh0909$'),
         'HOST': os.environ.get('DB_HOST', 'mirage.postgres.database.azure.com'),
         'PORT': os.environ.get('DB_PORT', '5432'),
 	}
 }
-
-
-
-#WKHTMLTOPDF_CMD_OPTIONS = {
-#    'page-size': 'Letter',
-#    'page-height': '11in',
-#    'page-width': '8.5in',
-#    'margin-top': '0.5in',
-#    'margin-right': '0.5in',
-#    'margin-bottom': '0.5in',
-#    'margin-left': '0.5in',
-#    'encoding': 'UTF-8',
-#    'no-outline': None
-#}
